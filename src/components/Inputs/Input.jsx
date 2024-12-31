6ed8b3ade82fe22069490940127533b5c73f9da8
unknown <abhishekmishra992016@gmail.com>
2024-12-31 13:30:41 +0530

    added flight details

--- src/components/Inputs/Input.jsx
+++ src/components/Inputs/Input.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
-const Input = ({ type }) => {
+const Input = ({ type, placeholder, id }) => {
     return (
         <input
             type={type}
-            id="from"
-            placeholder="From"
+            id={id}
+            placeholder={placeholder}
             className="w-full p-4 pl-12 text-gray-900 border border-gray-300 rounded-lg bg-white shadow-lg focus:ring-blue-500 focus:border-blue-500 transition duration-300"
         />
     )
--- src/components/SearchFlight/SearchFlight.jsx
+++ src/components/SearchFlight/SearchFlight.jsx
@@ -1,54 +1,206 @@
 import { FaSearch } from "react-icons/fa";
-import { FaPlaneDeparture, FaPlaneArrival, FaCalendarAlt } from "react-icons/fa";  // For the From, To, and Date icons
+import { FaPlaneDeparture, FaPlaneArrival, FaCalendarAlt } from "react-icons/fa";
 import Input from "../Inputs/Input";
 
-const SearchFlight = () => (
-    <div className="flex flex-col justify-center items-center text-white p-4">
-        <h1 className="text-4xl font-bold text-center mb-6 text-gradient">Search Flight</h1>
-        <div className="flex justify-center w-full bg-white p-8 rounded-lg shadow-2xl max-w-4xl">
-            <form className="w-full space-y-6">
-                <div className="flex flex-wrap space-x-6 justify-between">
-                    {/* From Input */}
-                    <div className="flex-1">
-                        <label htmlFor="from" className="block text-sm font-medium text-gray-900 mb-2">From</label>
-                        <div className="relative">
-                            <Input type="text" id="from" placeholder="From" />
-                            <FaPlaneDeparture className="absolute left-4 top-4 text-gray-500 transition-transform duration-300 group-hover:text-pink-500" />
+const SearchFlight = () => {
+
+    const flightDetails = [
+        {
+            id: 1,
+            airportCode: "DEL",
+            airportName: "Indira Gandhi International Airport",
+            city: "New Delhi",
+            location: "New Delhi",
+        },
+        {
+            id: 2,
+            airportCode: "BOM",
+            airportName: "Chhatrapati Shivaji Maharaj International Airport",
+            city: "Mumbai",
+            location: "Maharashtra",
+        },
+        {
+            id: 3,
+            airportCode: "BLR",
+            airportName: "Kempegowda International Airport",
+            city: "Bangalore",
+            location: "Karnataka",
+        },
+        {
+            id: 4,
+            airportCode: "MAA",
+            airportName: "Chennai International Airport",
+            city: "Chennai",
+            location: "Tamil Nadu",
+        },
+        {
+            id: 5,
+            airportCode: "CCU",
+            airportName: "Netaji Subhas Chandra Bose International Airport",
+            city: "Kolkata",
+            location: "West Bengal",
+        },
+        {
+            id: 6,
+            airportCode: "HYD",
+            airportName: "Rajiv Gandhi International Airport",
+            city: "Hyderabad",
+            location: "Telangana",
+        },
+        {
+            id: 7,
+            airportCode: "BLR",
+            airportName: "Kempegowda International Airport",
+            city: "Bengaluru",
+            location: "Karnataka",
+        },
+        {
+            id: 8,
+            airportCode: "COK",
+            airportName: "Cochin International Airport",
+            city: "Kochi",
+            location: "Kerala",
+        },
+        {
+            id: 9,
+            airportCode: "PNQ",
+            airportName: "Pune International Airport",
+            city: "Pune",
+            location: "Maharashtra",
+        },
+        {
+            id: 10,
+            airportCode: "GOI",
+            airportName: "Goa International Airport",
+            city: "Dabolim",
+            location: "Goa",
+        },
+        {
+            id: 11,
+            airportCode: "ATQ",
+            airportName: "Sri Guru Ram Dass Jee International Airport",
+            city: "Amritsar",
+            location: "Punjab",
+        },
+        {
+            id: 12,
+            airportCode: "TRV",
+            airportName: "Thiruvananthapuram International Airport",
+            city: "Thiruvananthapuram",
+            location: "Kerala",
+        },
+        {
+            id: 13,
+            airportCode: "IXC",
+            airportName: "Chandigarh International Airport",
+            city: "Chandigarh",
+            location: "Chandigarh",
+        },
+        {
+            id: 14,
+            airportCode: "IXB",
+            airportName: "Bagdogra Airport",
+            city: "Siliguri",
+            location: "West Bengal",
+        },
+        {
+            id: 15,
+            airportCode: "IXA",
+            airportName: "Agartala Airport",
+            city: "Agartala",
+            location: "Tripura",
+        },
+        {
+            id: 16,
+            airportCode: "IXR",
+            airportName: "Ranchi Airport",
+            city: "Ranchi",
+            location: "Jharkhand",
+        },
+        {
+            id: 17,
+            airportCode: "IXT",
+            airportName: "Tezpur Airport",
+            city: "Tezpur",
+            location: "Assam",
+        },
+        {
+            id: 18,
+            airportCode: "IXN",
+            airportName: "Nalbari Airport",
+            city: "Nalbari",
+            location: "Assam",
+        },
+        {
+            id: 19,
+            airportCode: "JAI",
+            airportName: "Jaipur International Airport",
+            city: "Jaipur",
+            location: "Rajasthan",
+        },
+        {
+            id: 20,
+            airportCode: "VDY",
+            airportName: "Vadodara Airport",
+            city: "Vadodara",
+            location: "Gujarat",
+        },
+        {
+            id: 21,
+            airportCode: "GAYA",
+            airportName: "Gaya Airport",
+            city: "Gaya",
+            location: "Bihar",
+        },
+    ];
+
+    return (
+        <div className="flex flex-col justify-center items-center text-white p-4">
+            <h1 className="text-4xl font-bold text-center mb-6 text-gradient">Search Flight</h1>
+            <div className="flex justify-center w-full bg-white p-8 rounded-lg shadow-2xl max-w-4xl">
+                <form className="w-full space-y-6">
+                    <div className="flex flex-wrap space-x-6 justify-between">
+                        {/* From Input */}
+                        <div className="flex-1">
+                            <label htmlFor="from" className="block text-sm font-medium text-gray-900 mb-2">From</label>
+                            <div className="relative">
+                                <Input type="text" id="from" placeholder="From" />
+                                <FaPlaneDeparture className="absolute left-4 top-4 text-gray-500 transition-transform duration-300 group-hover:text-pink-500" />
+                            </div>
+                        </div>
+
+                        {/* To Input */}
+                        <div className="flex-1">
+                            <label htmlFor="to" className="block text-sm font-medium text-gray-900 mb-2">To</label>
+                            <div className="relative">
+                                <Input type="text" id="to" placeholder="To" />
+                                <FaPlaneArrival className="absolute left-4 top-4 text-gray-500 transition-transform duration-300 group-hover:text-pink-500" />
+                            </div>
                         </div>
                     </div>
 
-                    {/* To Input */}
-                    <div className="flex-1">
-                        <label htmlFor="to" className="block text-sm font-medium text-gray-900 mb-2">To</label>
+                    {/* Date Input */}
+                    <div>
+                        <label htmlFor="date" className="block text-sm font-medium text-gray-900 mb-2">Date</label>
                         <div className="relative">
-                            <Input type="text" id="to" placeholder="To" />
-                            <FaPlaneArrival className="absolute left-4 top-4 text-gray-500 transition-transform duration-300 group-hover:text-pink-500" />
+                            <Input type="date" />
+                            <FaCalendarAlt className="absolute left-4 top-4 text-gray-500 transition-transform duration-300 group-hover:text-pink-500" />
                         </div>
                     </div>
-                </div>
 
-                {/* Date Input */}
-                <div>
-                    <label htmlFor="date" className="block text-sm font-medium text-gray-900 mb-2">Date</label>
-                    <div className="relative">
-                        <Input type="date" id="date" placeholder="Date" />
-                        <FaCalendarAlt className="absolute left-4 top-4 text-gray-500 transition-transform duration-300 group-hover:text-pink-500" />
+                    {/* Search Button */}
+                    <div className="text-center">
+                        <button
+                            type="submit"
+                            className="w-full bg-gradient-to-r from-pink-500 to-orange-500 text-white p-4 rounded-lg shadow-lg flex items-center justify-center space-x-3 hover:bg-gradient-to-l transition-all duration-300 transform hover:scale-105"
+                        >
+                            <FaSearch />
+                            <span>Search</span>
+                        </button>
                     </div>
-                </div>
-
-                {/* Search Button */}
-                <div className="text-center">
-                    <button
-                        type="submit"
-                        className="w-full bg-gradient-to-r from-pink-500 to-orange-500 text-white p-4 rounded-lg shadow-lg flex items-center justify-center space-x-3 hover:bg-gradient-to-l transition-all duration-300 transform hover:scale-105"
-                    >
-                        <FaSearch />
-                        <span>Search</span>
-                    </button>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
-);
-
+    )
+}
 export default SearchFlight;
